406efe26e65095936c0c9eaa4dbc80603ca6247b
amandacmarques <45610600+amandacmarques@users.noreply.github.com>
2020-04-23 15:43:45 -0700

    Resubmit ValidParenthesis code

--- ValidParenthesis.js
+++ ValidParenthesis.js
@@ -1,1 +1,52 @@
+/**
+ * @param {string} s The string of parenthesis to be broken up.
+ * @return {boolean} Whether the string of parenthesis was balanced or not.
+ */
+var isValid = function(s) {
+    let validChars = ['(','{','['];
+    let splitStr = s.split("");
+    let stack = [];
+    let c;
+    
+    for (let i = 0; i < splitStr.length; i++) {
+        if (validChars.includes(splitStr[i])) {
+            stack.push(splitStr[i]);
+        }
+        
+        if (stack.length == 0) {
+            return false;
+        }
+        
+        switch (splitStr[i]) 
+        { 
+        case ')':
+            c = stack.pop(); 
+            
+            if (c == '{' || c == '[') {
+                return false; 
+            }
+                
+            break; 
+  
+        case '}':
+            c = stack.pop();  
+                
+            if (c == '(' || c == '[') {
+                return false; 
+            }
+                
+            break; 
+  
+        case ']': 
+            c = stack.pop();
+                
+            if (c == '(' || c == '{') {
+                return false;
+            }
+            
+            break; 
+        } 
+    } 
 
+    return (stack.length == 0);
+};
